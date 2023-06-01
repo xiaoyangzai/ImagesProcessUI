6c2506fbf7c42972be6f58396b8781464248dfbb
Wang <yang4.wang@intel.com>
2023-06-01 11:10:24 +0800

    Create C# UI to check if the DLL built with C++ works as expected.
    Signed-off-by: Wang <yang4.wang@intel.com>

---
+++ ImageCenter/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ImageCenter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            load_status = new Label();
+            console = new RichTextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(380, 494);
+            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(232, 86);
+            button1.TabIndex = 0;
+            button1.Text = "Start Test";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // load_status
+            // 
+            load_status.AutoSize = true;
+            load_status.Location = new Point(75, 67);
+            load_status.Margin = new Padding(4, 0, 4, 0);
+            load_status.Name = "load_status";
+            load_status.Size = new Size(64, 25);
+            load_status.TabIndex = 1;
+            load_status.Text = "Status:";
+            load_status.Click += load_status_Click;
+            // 
+            // console
+            // 
+            console.Location = new Point(75, 126);
+            console.Name = "console";
+            console.Size = new Size(882, 343);
+            console.TabIndex = 3;
+            console.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1019, 750);
+            Controls.Add(console);
+            Controls.Add(load_status);
+            Controls.Add(button1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label load_status;
+        private RichTextBox console;
+        private byte[] imageBytes;
+    }
+}
---
+++ ImageCenter/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
+
+namespace ImageCenter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            imageBytes = File.ReadAllBytes("image.jpg");
+        }
+        // 委托类型
+        delegate void DebugCallbackDelegate(string message);
+
+        // 回调函数
+        private void DebugCallback(string message)
+        {
+            Console.WriteLine("DEBUG: " + message);
+            console.Text = message;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            [DllImport("C:\\Users\\ywang2\\source\\repos\\ImageProcessModule\\build\\imageProcessAlgor\\Debug\\Myimageops.dll")]
+            static extern void BaseFunctionTest(IntPtr data, int length);
+
+            [DllImport("C:\\Users\\ywang2\\source\\repos\\ImageProcessModule\\build\\imageProcessAlgor\\Debug\\Myimageops.dll")]
+            static extern void SetDebugCallback(DebugCallbackDelegate callback);
+            SetDebugCallback(new DebugCallbackDelegate(DebugCallback));
+            IntPtr ptr = Marshal.AllocHGlobal(30);
+            try
+            {
+                BaseFunctionTest(ptr, 20);
+                load_status.Text = "Status: Done!!";
+            }
+            catch (DllNotFoundException)
+            {
+                load_status.Text = "Status: DLL not found!!";
+            }
+            catch (EntryPointNotFoundException)
+            {
+                load_status.Text = "Status: Function not found!!";
+            }
+        }
+
+        private void msg_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void load_status_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
