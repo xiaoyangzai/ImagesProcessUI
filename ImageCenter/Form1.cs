a80cc8cafbcd189572dca00d2d465a022fd09a65
Yang Wang <wangyang.xjtu@foxmail.com>
2024-01-09 22:53:22 +0800

    Update.
    Signed-off-by: Yang Wang <wangyang.xjtu@foxmail.com>

--- ImageCenter/Form1.cs
+++ ImageCenter/Form1.cs
@@ -261,7 +261,7 @@ namespace ImageCenter
             [DllImport("image_process.dll")]
             static extern void SetDebugCallback(DebugCallbackDelegate callback);
             [DllImport("image_process.dll")]
-            static extern int CutTraceDetection(IntPtr source, int source_size, ref int traceCenterOffset, ref int tranceWidth, ref int maxTraceWith, ref int maxArea, int cutLineCenterY, int cutLineWidth, out IntPtr resultPtr);
+            static extern int CutTraceDetection(IntPtr source, int source_size, ref int traceCenterOffset, ref int tranceWidth, ref int maxTraceWith, ref int maxArea, ref int traceQuality, int cutLineCenterY, int cutLineWidth, out IntPtr resultPtr);
             SetDebugCallback(new DebugCallbackDelegate(DebugCallback));
 
             console.Text = "[Info] Calling CutTraceDetection function...\n";
@@ -271,10 +271,11 @@ namespace ImageCenter
                 int tranceWidth = -1;
                 int maxTraceWith = -1;
                 int maxArea = -1;
+                int traceQuality = -1;
                 IntPtr resultPtr = IntPtr.Zero;
                 int cutLineCencertLocY = (int)cutCenterLocY.Value;
                 int cutLineTraceWidth = (int)cutLineWidth.Value;
-                int ret = CutTraceDetection(Marshal.StringToHGlobalAnsi(imageBase64String), imageBase64String.Length, ref traceCenterOffset, ref tranceWidth, ref maxTraceWith, ref maxArea, cutLineCencertLocY, cutLineTraceWidth,  out resultPtr);
+                int ret = CutTraceDetection(Marshal.StringToHGlobalAnsi(imageBase64String), imageBase64String.Length, ref traceCenterOffset, ref tranceWidth, ref maxTraceWith, ref maxArea, ref traceQuality, cutLineCencertLocY, cutLineTraceWidth,  out resultPtr);
                 if (resultPtr != IntPtr.Zero)
                 {
                     string base64ImageData = Marshal.PtrToStringAnsi(resultPtr);
