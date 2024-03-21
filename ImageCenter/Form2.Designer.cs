4d94bdc0bca9df8e4f7b441ef48a671bf25c08ee
yang4 <yang4.wang@intel.com>
2024-03-21 23:51:10 +0800

    update-a
    Signed-off-by: yang4 <yang4.wang@intel.com>

---
+++ ImageCenter/Form2.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace ImageCenter
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox0 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            pictureBox12 = new PictureBox();
+            pictureBox13 = new PictureBox();
+            pictureBox14 = new PictureBox();
+            pictureBox15 = new PictureBox();
+            pictureBox16 = new PictureBox();
+            pictureBoxSouce = new PictureBox();
+            pictureBoxTarget = new PictureBox();
+            console = new RichTextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox0).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxSouce).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxTarget).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox0
+            // 
+            pictureBox0.Location = new Point(754, 22);
+            pictureBox0.Margin = new Padding(4, 3, 4, 3);
+            pictureBox0.Name = "pictureBox0";
+            pictureBox0.Size = new Size(254, 242);
+            pictureBox0.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox0.TabIndex = 9;
+            pictureBox0.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(1044, 22);
+            pictureBox1.Margin = new Padding(4, 3, 4, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(254, 242);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(1321, 22);
+            pictureBox2.Margin = new Padding(4, 3, 4, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(254, 242);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 11;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(754, 312);
+            pictureBox3.Margin = new Padding(4, 3, 4, 3);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(254, 242);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 14;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(1044, 312);
+            pictureBox4.Margin = new Padding(4, 3, 4, 3);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(254, 242);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 13;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(1321, 312);
+            pictureBox5.Margin = new Padding(4, 3, 4, 3);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(254, 242);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 12;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(754, 583);
+            pictureBox6.Margin = new Padding(4, 3, 4, 3);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(254, 242);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 17;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(1044, 580);
+            pictureBox7.Margin = new Padding(4, 3, 4, 3);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(254, 242);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 16;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Location = new Point(1321, 583);
+            pictureBox8.Margin = new Padding(4, 3, 4, 3);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(254, 242);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 15;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Location = new Point(1621, 23);
+            pictureBox9.Margin = new Padding(4, 3, 4, 3);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(246, 243);
+            pictureBox9.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox9.TabIndex = 18;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Location = new Point(1621, 313);
+            pictureBox10.Margin = new Padding(4, 3, 4, 3);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(246, 243);
+            pictureBox10.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox10.TabIndex = 19;
+            pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.Location = new Point(1621, 582);
+            pictureBox11.Margin = new Padding(4, 3, 4, 3);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(246, 243);
+            pictureBox11.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox11.TabIndex = 20;
+            pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.Location = new Point(1621, 883);
+            pictureBox12.Margin = new Padding(4, 3, 4, 3);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(246, 243);
+            pictureBox12.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox12.TabIndex = 21;
+            pictureBox12.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.Location = new Point(1901, 23);
+            pictureBox13.Margin = new Padding(4, 3, 4, 3);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(246, 243);
+            pictureBox13.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox13.TabIndex = 18;
+            pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            pictureBox14.Location = new Point(1901, 313);
+            pictureBox14.Margin = new Padding(4, 3, 4, 3);
+            pictureBox14.Name = "pictureBox14";
+            pictureBox14.Size = new Size(246, 243);
+            pictureBox14.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox14.TabIndex = 19;
+            pictureBox14.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            pictureBox15.Location = new Point(1901, 582);
+            pictureBox15.Margin = new Padding(4, 3, 4, 3);
+            pictureBox15.Name = "pictureBox15";
+            pictureBox15.Size = new Size(246, 243);
+            pictureBox15.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox15.TabIndex = 20;
+            pictureBox15.TabStop = false;
+            // 
+            // pictureBox16
+            // 
+            pictureBox16.Location = new Point(1901, 883);
+            pictureBox16.Margin = new Padding(4, 3, 4, 3);
+            pictureBox16.Name = "pictureBox16";
+            pictureBox16.Size = new Size(246, 243);
+            pictureBox16.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox16.TabIndex = 21;
+            pictureBox16.TabStop = false;
+            // 
+            // pictureBoxSouce
+            // 
+            pictureBoxSouce.Location = new Point(17, 18);
+            pictureBoxSouce.Margin = new Padding(4, 3, 4, 3);
+            pictureBoxSouce.Name = "pictureBoxSouce";
+            pictureBoxSouce.Size = new Size(721, 597);
+            pictureBoxSouce.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxSouce.TabIndex = 22;
+            pictureBoxSouce.TabStop = false;
+            // 
+            // pictureBoxTarget
+            // 
+            pictureBoxTarget.Location = new Point(136, 644);
+            pictureBoxTarget.Margin = new Padding(4, 3, 4, 3);
+            pictureBoxTarget.Name = "pictureBoxTarget";
+            pictureBoxTarget.Size = new Size(400, 362);
+            pictureBoxTarget.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxTarget.TabIndex = 23;
+            pictureBoxTarget.TabStop = false;
+            // 
+            // console
+            // 
+            console.Location = new Point(754, 845);
+            console.Name = "console";
+            console.Size = new Size(701, 252);
+            console.TabIndex = 24;
+            console.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(244, 1031);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 25;
+            button1.Text = "开始";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1924, 1170);
+            Controls.Add(button1);
+            Controls.Add(console);
+            Controls.Add(pictureBoxTarget);
+            Controls.Add(pictureBoxSouce);
+            Controls.Add(pictureBox16);
+            Controls.Add(pictureBox12);
+            Controls.Add(pictureBox15);
+            Controls.Add(pictureBox14);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox13);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox0);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox0).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxSouce).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxTarget).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox0;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox13;
+        private PictureBox pictureBox14;
+        private PictureBox pictureBox15;
+        private PictureBox pictureBox16;
+        private PictureBox pictureBoxSouce;
+        private PictureBox pictureBoxTarget;
+        private RichTextBox console;
+        private Button button1;
+    }
+}
---
+++ ImageCenter/Form2.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageCenter
+{
+    public partial class Form2 : Form
+    {
+        delegate void DebugCallbackDelegate(string message);
+        private void DebugCallback(string message)
+        {
+            console.Text += message;
+        }
+        [DllImport("image_process.dll")]
+        static extern int GetUniqueTargetHigh(IntPtr image, int imageSize, int targetCenterX, int targetCenterY, int targetWidth, int targetHigh, ref int offsetX, ref int offsetY, out IntPtr outputTargetImage, out IntPtr outputMatchedImage, UInt16 fontSize = 5);
+
+        [DllImport("image_process.dll")]
+        static extern void SetDebugCallback(DebugCallbackDelegate callback);
+        [DllImport("image_process.dll")]
+        static extern int MatchTargetCenter(IntPtr source, int source_size, IntPtr target, int target_size, ref int offsetX, ref int offsetY, out IntPtr resultPtr, UInt16 fontSize = 5, bool isShowOffset = true);
+
+        List<string> _sortedFilePathList;
+        List<string> base64ImageStringList;
+        List<PictureBox> pictureBoxesLeft;
+        string _sourceImage;
+        string _targetsString;
+        string _imageBase64String;
+        string base64TargetImageData;
+        int nextIndex = 0;
+        string[] targesStringList;
+
+        private bool Stage_1st(int targetCenteX, int targetCenterY, int targetWidth, int targetHigh)
+        {
+
+            console.Text = "[Info] check stage 1...\n";
+            try
+            {
+                console.Text += "[Info] GetUniqueTargetHigh() function...\n";
+                IntPtr targetPtr = IntPtr.Zero;
+                IntPtr resultPtr = IntPtr.Zero;
+                int offsetX = -1;
+                int offsetY = -1;
+                int ret = GetUniqueTargetHigh(Marshal.StringToHGlobalAnsi(_imageBase64String), _imageBase64String.Length, targetCenteX, targetCenterY, targetWidth, targetHigh, ref offsetX, ref offsetY, out targetPtr, out resultPtr);
+                if (resultPtr != IntPtr.Zero)
+                {
+                    string base64ImageData = Marshal.PtrToStringAnsi(resultPtr);
+                    byte[] imageBytes = Convert.FromBase64String(base64ImageData);
+                    MemoryStream ms = new MemoryStream(imageBytes);
+                    pictureBoxSouce.Image = Image.FromStream(ms);
+                }
+                if (ret < 0 || (ret < 90 && offsetX > 2 || offsetY > 2))
+                {
+                    console.Text += "\n[Error] Target is not valid :\n\tMatched quality: " + ret + "\n\toffset X: " + offsetX + "\n\toffsetY: " + offsetY + "\n";
+                    return false;
+                }
+                if (targetPtr != IntPtr.Zero)
+                {
+                    pictureBoxTarget.Image = null;
+                    base64TargetImageData = Marshal.PtrToStringAnsi(targetPtr);
+                    byte[] imageBytes = Convert.FromBase64String(base64TargetImageData);
+                    MemoryStream ms = new MemoryStream(imageBytes);
+                    pictureBoxTarget.Image = Image.FromStream(ms);
+                }
+                console.Text += "\n[Info] Calling GetUniqueTargetHigh() function...Done!\n";
+                console.Text += "[Info] Calling GetUniqueTargetHigh() function...Done\n";
+            }
+            catch (DllNotFoundException)
+            {
+                console.Text += "[Error] DLL not found!!\n";
+            }
+            catch (EntryPointNotFoundException)
+            {
+                console.Text += "[Error] Functior not found!!\n";
+            }
+            return true;
+        }
+        private bool Stage_2nd()
+        {
+            if (base64TargetImageData == "")
+            {
+                console.Text += "[Error] Target image is not valid.\n";
+                return false;
+            }
+            console.Text = "[Info] check stage 2...\n";
+            bool ret = false;
+
+            for (int i = 0; i < 9; i++)
+            {
+                string sourceBase64String = base64ImageStringList[i];
+                try
+                {
+                    int quality = -1;
+                    int offsetX = -1, offsetY = -1;
+                    IntPtr resultPtr = IntPtr.Zero;
+                    quality = MatchTargetCenter(Marshal.StringToHGlobalAnsi(sourceBase64String), sourceBase64String.Length, Marshal.StringToHGlobalAnsi(base64TargetImageData), base64TargetImageData.Length, ref offsetX, ref offsetY, out resultPtr, 7);
+                    if (quality < 0)
+                    {
+                        console.Text += "\n[Error] Failed to call MatchTargetCenter function. Exit Code: " + quality;
+                        return false;
+                    }
+                    if (resultPtr != IntPtr.Zero)
+                    {
+                        string base64ImageData = Marshal.PtrToStringAnsi(resultPtr);
+                        byte[] imageBytes = Convert.FromBase64String(base64ImageData);
+                        MemoryStream ms = new MemoryStream(imageBytes);
+                        pictureBoxesLeft[i].Image = Image.FromStream(ms);
+                    }
+                    //if (quality >= 60)
+                    //    return false;
+                }
+                catch (DllNotFoundException)
+                {
+                    console.Text = "[Error] DLL not found!!";
+                }
+                catch (EntryPointNotFoundException)
+                {
+                    console.Text = "[Error] Function MatcherTarget not found!!";
+                }
+            }
+            console.Text = "[Info] check stage 2...Done\n";
+            return true;
+        }
+        private bool Stage_3rd()
+        {
+            return true;
+        }
+        private bool CheckTarget(int targetCenteX, int targetCenterY, int targetWidth, int targetHigh)
+        {
+            // 1. get target image if it is unique
+            if (Stage_1st(targetCenteX, targetCenterY, targetWidth, targetHigh) == false)
+                return false;
+            // 2. 九宫格校验
+            if (Stage_2nd() == false)
+                return false;
+            // 3. 步进校验
+            return true;
+        }
+
+        public Form2(string sourceImage, string targetsString, List<string> sortedFilePathList)
+        {
+            InitializeComponent();
+            _sortedFilePathList = sortedFilePathList;
+            _sourceImage = sourceImage;
+            _targetsString = targetsString;
+
+            // source image
+            byte[] imageBytes = File.ReadAllBytes(_sourceImage);
+            _imageBase64String = Convert.ToBase64String(imageBytes);
+
+            // 九宫图
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            SetDebugCallback(new DebugCallbackDelegate(DebugCallback));
+            // 1. 九宫格
+            // 左上角-中上-右上角
+            // 左边- 中间-右边
+            // 左下角-中下-右小角
+            pictureBoxesLeft = new List<PictureBox> {
+                pictureBox0, pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5, pictureBox6, pictureBox7, pictureBox8,
+                pictureBox9, pictureBox10, pictureBox11, pictureBox12, pictureBox13, pictureBox14, pictureBox15, pictureBox16 };
+            base64ImageStringList = new List<string>();
+            for (int i = 0; i < 17; i++)
+            {
+                // pictureBoxesLeft[i].Image = Image.FromFile(_sortedFilePathList[i]);
+                byte[] imageBytes = File.ReadAllBytes(_sortedFilePathList[i]);
+                base64ImageStringList.Add(Convert.ToBase64String(imageBytes));
+            }
+            nextIndex = 0;
+            targesStringList = _targetsString.Split(',');
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var box in pictureBoxesLeft)
+            {
+                box.Image = null;
+            }
+            pictureBoxSouce.Image = null;
+            pictureBoxTarget.Image = null;
+            base64TargetImageData = "";
+            console.Text = "Parse the target string...index: " + nextIndex + "\n";
+            string targe = targesStringList[nextIndex++];
+            {
+                string[] subItems = targe.Split('-');
+                int targetCenterX = Convert.ToInt32(subItems[0]);
+                int targetCenterY = Convert.ToInt32(subItems[1]);
+                int targetWidth = Convert.ToInt32(subItems[2]);
+                int targetHigh = Convert.ToInt32(subItems[3]);
+                console.Text += "check target located at " + targetCenterX + "*" + targetCenterY + " with size " + targetWidth + "*" + targetHigh + "...\n";
+                button1.Enabled = false;
+                bool ret = CheckTarget(targetCenterX, targetCenterY, targetWidth, targetHigh);
+                if (ret == false)
+                {
+                    console.Text += "\nInvalid target\n";
+                }
+                console.Text += "check target located at " + targetCenterX + "*" + targetCenterY + " with size " + targetWidth + "*" + targetHigh + "...Done\n";
+                button1.Enabled = true;
+            }
+
+        }
+    }
+}
